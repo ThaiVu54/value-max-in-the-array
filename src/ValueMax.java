183036f26fc28807823f86d6987b560fc26bac57
ThaiVu54 <thai0165436944@gmail.com>
2021-09-17 16:05:09 +0700

    value max

--- src/ValueMax.java
+++ src/ValueMax.java
@@ -5,23 +5,25 @@ public class ValueMax {
         //* khai báo các biến, nhập và kiểm tra kích thước mảng
         int size;
         int[] array;
-        Scanner sc = new Scanner(System.in);
-        size = sc.nextInt();
-        if (size > 20) {
-            System.out.println("size should not exceed 20");
-        } while (size > 20) ;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
 
         //* nhập giá trị cho các phần tử của mảng
         array = new int[size];
         int i = 0;
         while (i < array.length) {
-            System.out.print("enter element" + (i + 1) + " : ");
-            array[i] = sc.nextInt();
+            System.out.print("Enter element" + (i + 1) + " : ");
+            array[i] = scanner.nextInt();
             i++;
         }
 
         //*in ra danh sách tài sản đã nhập
-        System.out.print("property list: ");
+        System.out.print("Property list: ");
         for (int j = 0; j < array.length; j++) {
             System.out.print(array[j] + "\t");
         }
@@ -35,6 +37,6 @@ public class ValueMax {
                 index = j + 1;
             }
         }
-        System.out.println("the largest property value in the list is " + max + " , at position" + index);
+        System.out.println("The largest property value in the list is " + max + " ,at position " + index);
     }
 }
